3f4ff6762226de4ad93241cb876c43575b4f2d29
derdem7 <derdemm7@gmail.com>
2022-05-21 19:12:03 +0300

    Odev10 tamamlandı

--- app.js
+++ app.js
@@ -31,9 +31,15 @@ app.get('/about', (req, res) => {
     res.render('about');
 })
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id',async (req,res)=>{
+    const post = await Post.findById(req.params.id);
+
+    res.render('post',{post});
 })
+
+// app.get('/post', (req, res) => {
+//     res.render('post');
+// })
 
 app.get('/add_post', (req, res) => {
     res.render('add_post');
